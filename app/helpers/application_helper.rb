63ad08120ab8c5883d94d22ebdb969bf0034eeb1
bluebitch <wzc345@gmail.com>
2013-11-26 07:57:36 -0800

    修正错误。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 	
 	def return_response(api_return)
 		  	respond_to do |format|
-		  		format.html { render json: api_return }
 		  		format.json { render json: api_return }
 		  	end
 		end
