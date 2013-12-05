60a7d4fecb5ffc4a81291fcbee1ac76357de23c1
bluebitch <wzc345@gmail.com>
2013-12-04 21:07:46 -0800

    two layered search.

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -9,9 +9,14 @@ class AudioController < ApplicationController
 
 	def self.init_syllables_hashtable
 		puts 'init_syllables_hashtable'
+
+    if !ActiveRecord::Base.connection.table_exists? 'audios' then return end
+    puts Audio.all
+
 		Audio.all.each do |audio|
 			audio.segmented_semantic.split.each do |keyword|
 				puts keyword
+        if keyword == 'null' then next end
         hash = SyllablesProcessing.calculate_noraml_han_word_hash(keyword)
         puts hash
         if @@syllables_hashtable.has_key?(hash)
@@ -27,7 +32,11 @@ class AudioController < ApplicationController
     @audio = Audio.new(audio_params)
     raw_seg = TextSegmentation.text_segment(@audio.complete_semantic)
     key_seg = SemanticProcessing.semantic_process(raw_seg)
-    @audio.segmented_semantic = key_seg
+    if !key_seg.empty?
+      @audio.segmented_semantic = key_seg
+    else
+      @audio.segmented_semantic = 'null'
+    end
     @audio.audio = params[:audio_file]
     if @audio.save
       # Handle a successful save.
@@ -35,6 +44,18 @@ class AudioController < ApplicationController
       re = ApiReturn.new("000")
    		return_response(re)
    		puts re
+
+      @audio.segmented_semantic.split.each do |keyword|
+        puts keyword
+        if keyword == 'null' then next end
+        hash = SyllablesProcessing.calculate_noraml_han_word_hash(keyword)
+        puts hash
+        if @@syllables_hashtable.has_key?(hash)
+          @@syllables_hashtable[hash] << @audio.id
+        else
+          @@syllables_hashtable[hash] = [@audio.id]
+        end
+      end
     else
     	puts @audio.errors.full_messages
     	render text: @audio.errors.full_messages
@@ -52,19 +73,58 @@ class AudioController < ApplicationController
   end
 
   def search
+    # first layer search
     keywords = params[:keywords]
-    result = []
+    first_result = []
     raw_seg = TextSegmentation.text_segment(keywords)
     key_seg = SemanticProcessing.semantic_process(raw_seg)
     key_seg.split.each do |keyword|
       hash = SyllablesProcessing.calculate_noraml_han_word_hash(keyword)
-      if result.empty?
-        result |= @@syllables_hashtable[hash]
+      if first_result.empty?
+        first_result |= @@syllables_hashtable[hash]
       else
-        result &= @@syllables_hashtable[hash]
+        first_result &= @@syllables_hashtable[hash]
       end
     end
-    render json: result
+
+    puts "first_result"
+    puts first_result
+
+    audios = []
+    first_result.each do |audio_id|
+      audios << Audio.find(audio_id)
+    end
+
+    # second layer search 
+    puts "audios"
+    puts audios
+
+    audios_weight = {}
+    search_tone_hashs = []
+    key_seg.split.each do |keyword|
+      search_tone_hashs << SyllablesProcessing.calculate_tone_han_word_hash(keyword)
+    end
+
+    puts "search_tone_hashs"
+    puts search_tone_hashs
+
+    audios.each do |audio|
+      audios_weight[audio] = 0
+      audio.segmented_semantic.split.each do |seg_key|
+        tone_hash = SyllablesProcessing.calculate_tone_han_word_hash(seg_key)
+        if search_tone_hashs.include?(tone_hash) then audios_weight[audio] += 10 end
+      end
+    end
+
+    puts "audios_weight"
+    puts audios_weight
+
+    second_result = []
+    audios_weight.sort_by{|audio, weight| weight}[0..[5, audios_weight.length].min].each do |audio_weight_pair|
+      second_result << audio_weight_pair[0]
+    end
+
+    render json: second_result
   end
 
 	private 
